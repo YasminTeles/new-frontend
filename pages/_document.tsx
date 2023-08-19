36d0675dce873cb706d99825d230419a394d8fd2
Yasmin Teles <teles.yasmin.tech@gmail.com>
2023-08-19 15:59:06 -0300

    feat: add metatags in header

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,10 @@ function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="author" content="Yasmin Teles" />
+        <meta name="description" content="The challenge was designed and built by Yasmin Teles." />
+        <meta name="image" content={`https://${process.env.VERCEL_URL}/images/cover.png`} />
+
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
