2512cedcc732d3a03e5b0b0a6d767bd99c8d9372
Yasmin Teles <teles.yasmin.tech@gmail.com>
2023-08-19 14:07:56 -0300

    feat: add custom favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,9 @@ function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <body>
