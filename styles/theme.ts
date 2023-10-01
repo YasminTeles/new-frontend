8ef255b0e0718b2595a9a1991b6d1b64f4ed43f7
Yasmin Teles <teles.yasmin.tech@gmail.com>
2023-10-01 14:11:32 -0300

    feat: add roboto fonts

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,20 @@
-import { createTheme, responsiveFontSizes } from "@mui/material/styles"
+/* eslint-disable import/no-mutable-exports */
+/* eslint-disable import/prefer-default-export */
 
-let theme = createTheme({})
+import { createTheme, responsiveFontSizes } from "@mui/material/styles"
+import { Roboto } from "next/font/google"
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+})
+
+let theme = createTheme({
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+})
 
 theme = responsiveFontSizes(theme)
 
