ed5f6c696cd2514e90bcfa5782ee21ae44ba8632
Yasmin Teles <teles.yasmin.tech@gmail.com>
2024-12-29 15:28:17 -0300

    chore: update next configs

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  optimizeFonts: true,
-  output: "standalone",
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
 }
 
 module.exports = nextConfig
